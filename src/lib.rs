74d3adafc26f992c4ce826b63d126288cba0babf
Aatif Syed <aatifsyedyp@gmail.com>
2024-07-04 23:52:41 +0100

    doc: design

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 //! A transcription of types from the [`JSON-RPC 2.0` Specification](https://www.jsonrpc.org/specification).
 //!
 //! > When quoted, the specification will appear as blockquoted text, like so.
+//!
+//! # Design
+//! - All structs are owned (i.e, there is no borrowing of data from the [`Deserializer`](serde::Deserializer)),
+//!   to facilitate ergonomics.
+//! - Appearances of dynamic JSON [`Value`]s are parameterised out, to allow
+//!   deferred serialization using, i.e [RawValue](https://docs.rs/serde_json/latest/serde_json/value/struct.RawValue.html).
 
 use std::{
     borrow::Cow,
